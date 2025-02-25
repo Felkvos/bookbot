5abde7328ca90dd8cde5135c3d7af0a84f0d606c
atramos12@gmail.com <your.email@example.com>
2025-02-25 05:49:09 -0500

    C: Added character count to stats and main.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import string
-from stats import get_num_words
+from stats import get_num_words, character_count
 def get_book_text(filepath):
     with open(filepath) as f:
         contents = f.read()
@@ -9,6 +9,8 @@ def main():
     path = "books/frankenstein.txt"
     book = (get_book_text(path)) 
     word_count = get_num_words(book)
+    letter_count = character_count(book)
     print(f"{word_count} words found in the document")
+    print(letter_count)
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,15 @@ def get_num_words(text):
     word_split = text.split()
     word_count = (len(word_split))
     return word_count
+def character_count(letters):
+    char_counts = {}
+    lower_c = letters.lower()
+    for char in lower_c:
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
+    return char_counts
+            
+
+
