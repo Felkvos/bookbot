17ae3a3c8aac36082bb434748cded4fbe56f8b66
atramos12@gmail.com <your.email@example.com>
2025-02-26 05:22:11 -0500

    D: added listed character count without alpha

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import string
-from stats import get_num_words, character_count
+from stats import get_num_words, character_count, sort_chars
 def get_book_text(filepath):
     with open(filepath) as f:
         contents = f.read()
@@ -10,7 +10,20 @@ def main():
     book = (get_book_text(path)) 
     word_count = get_num_words(book)
     letter_count = character_count(book)
-    print(f"{word_count} words found in the document")
-    print(letter_count)
+    sorted_chars = sort_chars(letter_count)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+        
+    for char_dict in sorted_chars:
+        for char, count in char_dict.items():
+            if char.isalpha():
+                print(f"{char}: {count}")
 
+    print("============= END ===============")
+                    
+    
+    
 main()
--- stats.py
+++ stats.py
@@ -11,6 +11,16 @@ def character_count(letters):
         else:
             char_counts[char] = 1
     return char_counts
-            
 
+def sort_on(dict_item):
+    return list(dict_item.values())[0]
+
+def sort_chars(char_dict):
+    chars_list = []
+    for char, count in char_dict.items():
+        new_dict = {char: count}
+        chars_list.append(new_dict)
+
+    chars_list.sort(reverse=True, key=sort_on)
+    return chars_list
 
